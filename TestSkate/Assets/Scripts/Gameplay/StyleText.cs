0c0ae735f2788b83aa08778d5a6a0e1953ab3c33
Pess-dev <kirillovjahik@mail.ru>
2024-06-16 02:48:12 +0300

    Add Spline tool

--- TestSkate/Assets/Scripts/Gameplay/StyleText.cs
+++ TestSkate/Assets/Scripts/Gameplay/StyleText.cs
@@ -28,15 +28,15 @@ public class StyleText : MonoBehaviour
         }
     }
 //D	C	B	A	S	SS	SSS
-    public List<Style> styles = new List<Style>(){
+    List<Style> styles = new List<Style>(){
         new Style("", 0f, Color.white, 36),
-        new Style("D", 0.3f, Color.white, 36),
-        new Style("C", 0.4f, Color.blue, 50),
-        new Style("B", 0.5f, Color.yellow, 60),
-        new Style("A", 0.6f, new Color(1, 0.4f, 0), 70),
-        new Style("S", 0.80f, Color.red, 80),
-        new Style("SS", 0.90f, Color.red, 90),
-        new Style("SSS", 0.95f, Color.red, 100)
+        new Style("Dull", 0.3f, Color.white, 36),
+        new Style("Cool!", 0.4f, Color.blue, 50),
+        new Style("Bravo!", 0.5f, Color.yellow, 60),
+        new Style("Awesome!", 0.6f, new Color(1, 0.4f, 0), 70),
+        new Style("Sweet!", 0.80f, Color.red, 80),
+        new Style("SShowtime!!", 0.90f, Color.red, 90),
+        new Style("SSStylish!!!", 0.95f, Color.red, 100)
     };
 
      private Rigidbody _rb;
